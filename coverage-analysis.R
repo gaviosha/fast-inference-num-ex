794113fbc1c88d7649cc7122aad8bd54eacbf04a
Shakeel95 <41747714+Shakeel95@users.noreply.github.com>
2023-05-03 23:42:59 +0100

    sims

--- coverage-analysis.R
+++ coverage-analysis.R
@@ -36,238 +36,238 @@ registerDoParallel(cl)
 # N1
 #-------------------
 
-# 
-# ## degree = 0 
-# ##
-# 
-# N1_deg_0 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
-#   {
-#     source("methods-wrappers.R")
-#     
-#     progress_file <- file("progress.txt")
-#     
-#     writeLines(paste("running itteration ",ii, "; noise = N1, degree = 0"),progress_file)
-#     
-#     xx <- rnorm(nn)
-#     
-#     out <- numeric(9)
-#     
-#     out[1] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0 
-#     out[2] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[3] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[4] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
-#     
-#     out[5] <- nrow(nsp_poly(xx, alpha = alpha, deg = 0)$intervals) == 0 
-#     
-#     out[6] <- 0 # Bain and Perron TO-DO
-#     
-#     out[7] <- nrow(uniscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
-#     out[8] <- nrow(multiscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
-#     
-#     out[9] <- nrow(smuce_ints(xx, alpha = alpha)$intervals) == 0
-#     
-#     out 
-#   }
-# 
-# N1_deg_0 <- apply(N1_deg_0, 1, mean)
-# 
-# 
-# ## degree = 1 
-# ##
-# 
-# N1_deg_1 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
-#   {
-#     progress_file <- file("progress.txt")
-#     
-#     writeLines(paste("running itteration ",ii, "; noise = N1, degree = 1"),progress_file)
-#     
-#     xx <- rnorm(nn)
-#     
-#     out <- numeric(9)
-#     
-#     out[1] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0 
-#     out[2] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[3] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[4] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
-#     
-#     out[5] <- nrow(nsp_poly(xx, alpha = alpha, deg = 1)$intervals) == 0 
-#     
-#     out[6] <- 0 # Bain and Perron TO-DO
-#     
-#     out
-#   }
-# 
-# N1_deg_1 <- apply(N1_deg_1, 1, mean)
-# 
-# 
-# ## degree = 2 
-# ##
-# 
-# N1_deg_2 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
-#   {
-#     progress_file <- file("progress.txt")
-#     
-#     writeLines(paste("running itteration ",ii, "; noise = N1, degree = 2"),progress_file)
-#     
-#     xx <- rnorm(nn)
-#     
-#     out <- numeric(9)
-#     
-#     out[1] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0 
-#     out[2] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[3] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[4] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
-#     
-#     out[5] <- nrow(nsp_poly(xx, alpha = alpha, deg = 2)$intervals) == 0 
-#     
-#     out[6] <- 0 # Bain and Perron TO-DO
-#     
-#     out
-#   }
-# 
-# N1_deg_2 <- apply(N1_deg_2, 1, mean)
-# 
-# 
-# ## Save outputs
-# ##
-# 
-# N1_coverage <- data.frame(cbind(N1_deg_0, N1_deg_1, N1_deg_2))
-# 
-# save(N1_coverage, file = "RData/N1-coverage")
-# 
-# 
-# rownames(N1_coverage) <- c(diff_names, "NSP", other_names)
-# 
-# colnames(N1_coverage) <- degrees
-# 
-# N1_coverage[6,] <- "-"
-# 
-# N1_coverage[7:9,2:3] <- "-"
-# 
-# print(
-#   xtable(N1_coverage, align = "|l|c|c|c|"), 
-#   file = "tables/N1-coverage.tex",
-#   floating = FALSE
-# )
-# 
-# #-------------------
-# # N2
-# #-------------------
-# 
-# 
-# ## degree = 0 
-# ##
-# 
-# N2_deg_0 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
-#   {
-#     source("methods-wrappers.R")
-#     
-#     progress_file <- file("progress.txt")
-#     
-#     writeLines(paste("running itteration ",ii, "; noise = N2, degree = 0"),progress_file)
-#     
-#     xx <- sqrt(3/5) * rt(nn, df = 5)
-#     
-#     out <- numeric(9)
-#     
-#     out[1] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0 
-#     out[2] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[3] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[4] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
-#     
-#     out[5] <- nrow(nsp_poly_selfnorm(xx, alpha = alpha, deg = 0)$intervals) == 0 
-#     
-#     out[6] <- 0 # Bain and Perron TO-DO
-#     
-#     out[7] <- nrow(uniscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
-#     out[8] <- nrow(multiscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
-#     
-#     out[9] <- nrow(smuce_ints(xx, alpha = alpha)$intervals) == 0
-#     
-#     out 
-#   }
-# 
-# N2_deg_0 <- apply(N2_deg_0, 1, mean)
-# 
-# 
-# ## degree = 1 
-# ##
-# 
-# N2_deg_1 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
-#   {
-#     progress_file <- file("progress.txt")
-#     
-#     writeLines(paste("running itteration ",ii, "; noise = N2, degree = 1"),progress_file)
-#     
-#     xx <- sqrt(3/5) * rt(nn, df = 5)
-#     
-#     out <- numeric(9)
-#     
-#     out[1] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0 
-#     out[2] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[3] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[4] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
-#     
-#     out[5] <- nrow(nsp_poly_selfnorm(xx, alpha = alpha, deg = 1)$intervals) == 0 
-#     
-#     out[6] <- 0 # Bain and Perron TO-DO
-#     
-#     out
-#   }
-# 
-# N2_deg_1 <- apply(N2_deg_1, 1, mean)
-# 
-# 
-# ## degree = 2 
-# ##
-# 
-# N2_deg_2 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
-#   {
-#     progress_file <- file("progress.txt")
-#     
-#     writeLines(paste("running itteration ",ii, "; noise = N2, degree = 2"),progress_file)
-#     
-#     xx <- sqrt(3/5) * rt(nn, df = 5)
-#     
-#     out <- numeric(9)
-#     
-#     out[1] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0 
-#     out[2] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[3] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0 
-#     out[4] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
-#     
-#     out[5] <- nrow(nsp_poly_selfnorm(xx, alpha = alpha, deg = 2)$intervals) == 0 
-#     
-#     out[6] <- 0 # Bain and Perron TO-DO
-#     
-#     out
-#   }
-# 
-# N2_deg_2 <- apply(N2_deg_2, 1, mean)
-# 
-# 
-# ## Save outputs
-# ##
-# 
-# N2_coverage <- data.frame(cbind(N2_deg_0, N2_deg_1, N2_deg_2))
-# 
-# save(N2_coverage, file = "RData/N2-coverage")
-# 
-# 
-# rownames(N2_coverage) <- c(diff_names, "NSP-SN", other_names)
-# 
-# colnames(N2_coverage) <- degrees
-# 
-# N2_coverage[6,] <- "-"
-# 
-# N2_coverage[7:9,2:3] <- "-"
-# 
-# print(
-#   xtable(N2_coverage, align = "|l|c|c|c|"), 
-#   file = "tables/N2-coverage.tex",
-#   floating = FALSE
-# )
-# 
+
+## degree = 0
+##
+
+N1_deg_0 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
+  {
+    source("methods-wrappers.R")
+
+    progress_file <- file("progress.txt")
+
+    writeLines(paste("running itteration ",ii, "; noise = N1, degree = 0"),progress_file)
+
+    xx <- rnorm(nn)
+
+    out <- numeric(9)
+
+    out[1] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0
+    out[2] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[3] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[4] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+
+    out[5] <- nrow(nsp_poly(xx, alpha = alpha, deg = 0)$intervals) == 0
+
+    out[6] <- 0 # Bain and Perron TO-DO
+
+    out[7] <- nrow(uniscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
+    out[8] <- nrow(multiscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
+
+    out[9] <- nrow(smuce_ints(xx, alpha = alpha)$intervals) == 0
+
+    out
+  }
+
+N1_deg_0 <- apply(N1_deg_0, 1, mean)
+
+
+## degree = 1
+##
+
+N1_deg_1 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
+  {
+    progress_file <- file("progress.txt")
+
+    writeLines(paste("running itteration ",ii, "; noise = N1, degree = 1"),progress_file)
+
+    xx <- rnorm(nn)
+
+    out <- numeric(9)
+
+    out[1] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0
+    out[2] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[3] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[4] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+
+    out[5] <- nrow(nsp_poly(xx, alpha = alpha, deg = 1)$intervals) == 0
+
+    out[6] <- 0 # Bain and Perron TO-DO
+
+    out
+  }
+
+N1_deg_1 <- apply(N1_deg_1, 1, mean)
+
+
+## degree = 2
+##
+
+N1_deg_2 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
+  {
+    progress_file <- file("progress.txt")
+
+    writeLines(paste("running itteration ",ii, "; noise = N1, degree = 2"),progress_file)
+
+    xx <- rnorm(nn)
+
+    out <- numeric(9)
+
+    out[1] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0
+    out[2] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[3] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[4] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+
+    out[5] <- nrow(nsp_poly(xx, alpha = alpha, deg = 2)$intervals) == 0
+
+    out[6] <- 0 # Bain and Perron TO-DO
+
+    out
+  }
+
+N1_deg_2 <- apply(N1_deg_2, 1, mean)
+
+
+## Save outputs
+##
+
+N1_coverage <- data.frame(cbind(N1_deg_0, N1_deg_1, N1_deg_2))
+
+save(N1_coverage, file = "RData/N1-coverage")
+
+
+rownames(N1_coverage) <- c(diff_names, "NSP", other_names)
+
+colnames(N1_coverage) <- degrees
+
+N1_coverage[6,] <- "-"
+
+N1_coverage[7:9,2:3] <- "-"
+
+print(
+  xtable(N1_coverage, align = "|l|c|c|c|"),
+  file = "tables/N1-coverage.tex",
+  floating = FALSE
+)
+
+#-------------------
+# N2
+#-------------------
+
+
+## degree = 0
+##
+
+N2_deg_0 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
+  {
+    source("methods-wrappers.R")
+
+    progress_file <- file("progress.txt")
+
+    writeLines(paste("running itteration ",ii, "; noise = N2, degree = 0"),progress_file)
+
+    xx <- sqrt(3/5) * rt(nn, df = 5)
+
+    out <- numeric(9)
+
+    out[1] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0
+    out[2] <- nrow(diffInf(xx, degree = 0, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[3] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[4] <- nrow(diffInf(xx, degree = 0, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+
+    out[5] <- nrow(nsp_poly_selfnorm(xx, alpha = alpha, deg = 0)$intervals) == 0
+
+    out[6] <- 0 # Bain and Perron TO-DO
+
+    out[7] <- nrow(uniscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
+    out[8] <- nrow(multiscale_mosum_ints(xx, alpha = alpha, min_width = 20)$intervals) == 0
+
+    out[9] <- nrow(smuce_ints(xx, alpha = alpha)$intervals) == 0
+
+    out
+  }
+
+N2_deg_0 <- apply(N2_deg_0, 1, mean)
+
+
+## degree = 1
+##
+
+N2_deg_1 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
+  {
+    progress_file <- file("progress.txt")
+
+    writeLines(paste("running itteration ",ii, "; noise = N2, degree = 1"),progress_file)
+
+    xx <- sqrt(3/5) * rt(nn, df = 5)
+
+    out <- numeric(9)
+
+    out[1] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0
+    out[2] <- nrow(diffInf(xx, degree = 1, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[3] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[4] <- nrow(diffInf(xx, degree = 1, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+
+    out[5] <- nrow(nsp_poly_selfnorm(xx, alpha = alpha, deg = 1)$intervals) == 0
+
+    out[6] <- 0 # Bain and Perron TO-DO
+
+    out
+  }
+
+N2_deg_1 <- apply(N2_deg_1, 1, mean)
+
+
+## degree = 2
+##
+
+N2_deg_2 <- foreach(ii=1:KK, .combine = "cbind") %dopar%
+  {
+    progress_file <- file("progress.txt")
+
+    writeLines(paste("running itteration ",ii, "; noise = N2, degree = 2"),progress_file)
+
+    xx <- sqrt(3/5) * rt(nn, df = 5)
+
+    out <- numeric(9)
+
+    out[1] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = FALSE, alpha = alpha)$intervals) == 0
+    out[2] <- nrow(diffInf(xx, degree = 2, noise_type = "gaussian", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[3] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = FALSE, alpha = alpha, min_scale = 20)$intervals) == 0
+    out[4] <- nrow(diffInf(xx, degree = 2, noise_type = "non_gaussian_dependent", dependent_noise = TRUE, alpha = alpha, min_scale = 20)$intervals) == 0
+
+    out[5] <- nrow(nsp_poly_selfnorm(xx, alpha = alpha, deg = 2)$intervals) == 0
+
+    out[6] <- 0 # Bain and Perron TO-DO
+
+    out
+  }
+
+N2_deg_2 <- apply(N2_deg_2, 1, mean)
+
+
+## Save outputs
+##
+
+N2_coverage <- data.frame(cbind(N2_deg_0, N2_deg_1, N2_deg_2))
+
+save(N2_coverage, file = "RData/N2-coverage")
+
+
+rownames(N2_coverage) <- c(diff_names, "NSP-SN", other_names)
+
+colnames(N2_coverage) <- degrees
+
+N2_coverage[6,] <- "-"
+
+N2_coverage[7:9,2:3] <- "-"
+
+print(
+  xtable(N2_coverage, align = "|l|c|c|c|"),
+  file = "tables/N2-coverage.tex",
+  floating = FALSE
+)
+
 
 #-------------------
 # N3
@@ -499,12 +499,12 @@ rownames(N4_coverage) <- c(diff_names,"NSP-AR" ,"NSP-SN", other_names)
 
 colnames(N4_coverage) <- degrees
 
-N2_coverage[7,] <- "-"
+N4_coverage[7,] <- "-"
 
-N2_coverage[8:10,2:3] <- "-"
+N4_coverage[8:10,2:3] <- "-"
 
 print(
-  xtable(N2_coverage, align = "|l|c|c|c|", digits = c(0,0,0,0)), 
+  xtable(N4_coverage, align = "|l|c|c|c|", digits = c(0,0,0,0)), 
   file = "tables/N4-coverage.tex",
   floating = FALSE
   )
