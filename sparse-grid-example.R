769ccf049e6d920bf6eb096fd4026ba10e14b518
Shakeel95 <s.a.gavioli-akilagun@lse.ac.uk>
2023-06-08 12:04:38 +0100

    sparse gird example plot

---
+++ sparse-grid-example.R
@@ -0,0 +1,37 @@
+
+## params
+##
+
+nn <- 10
+
+aa <- sqrt(2)
+
+eps <- 0.02
+
+n_scales <- floor(logb(nn/2,aa))
+
+
+## base plot
+##
+
+plot(NULL, xlim = c(0,nn), ylim = c(0,1), axes = FALSE, ann = FALSE)
+
+axis(1)
+
+
+## plot scales
+##
+
+for (kk in 1:n_scales)
+{
+  ww <- floor(aa**kk)
+  
+  yy <- seq(kk/n_scales,(kk-1)/n_scales, length.out = nn - ww)
+  
+  axis(2, at = c((kk-1)/n_scales + eps, kk/n_scales - eps), ann = FALSE, labels = FALSE)
+  
+  for (ll in 1:(nn - ww))
+  {
+    segments(x0 = ll, x1 = ll+ww, y0 = yy[ll], y1 = yy[ll], lwd = 3)
+  }
+}
